6b7373e728641e5dddf10627ef6d3b9a8565caf4
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2025-09-08 12:37:48 +0200

    Update crypto.ts

--- src/Utils/crypto.ts
+++ src/Utils/crypto.ts
@@ -5,7 +5,7 @@ import { KEY_BUNDLE_TYPE } from '../Defaults'
 import type { KeyPair } from '../Types'
 
 // insure browser & node compatibility
-const { subtle } = globalThis.crypto
+const subtle: SubtleCrypto = (globalThis.crypto ?? require('crypto').webcrypto).subtle
 
 /** prefix version byte to the pub keys, required for some curve crypto functions */
 export const generateSignalPubKey = (pubKey: Uint8Array | Buffer) =>
